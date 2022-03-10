ddfd64f2d8d3ab77d040743bf7b4a73471232ebe
irenarch <irinakjoseva1@gmail.com>
2022-03-10 11:11:25 +0100

    Functioning basic security config

--- src/main/java/irinakjoseva/vecnamoda/configuration/WebSecurityConfig.java
+++ src/main/java/irinakjoseva/vecnamoda/configuration/WebSecurityConfig.java
@@ -3,22 +3,22 @@ package irinakjoseva.vecnamoda.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(
-        prePostEnabled = true,
-        securedEnabled = true,
-        jsr250Enabled = true
-)
-public class WebSecurityConfig extends GlobalMethodSecurityConfiguration {
 
-    @Autowired
-    UserDetailsServiceImpl userDetailsService;
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/**").permitAll();
+    }
 
 }
--- src/main/java/irinakjoseva/vecnamoda/controller/UserController.java
+++ src/main/java/irinakjoseva/vecnamoda/controller/UserController.java
@@ -4,7 +4,6 @@ import irinakjoseva.vecnamoda.controller.dto.UserDto;
 import irinakjoseva.vecnamoda.model.User;
 import irinakjoseva.vecnamoda.service.UserService;
 
-import org.springframework.security.core.Authentication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -13,12 +12,17 @@ import javax.validation.Valid;
 import java.util.HashMap;
 
 @Controller
-@RequestMapping("api/user")
+@RequestMapping("api/users")
 public class UserController {
     private final UserService service;
 
     public UserController(UserService service) {
         this.service = service;
+    }
+
+    @GetMapping({ "/hello" })
+    public ResponseEntity firstPage() {
+        return ResponseEntity.ok("Hello world");
     }
 
     @PostMapping(value = "/register")
@@ -30,13 +34,6 @@ public class UserController {
     public ResponseEntity<User> getByUsername(@PathVariable("username") String username) {
         User user = this.service.getByUsername(username);
         return ResponseEntity.ok(user);
-    }
-
-    @GetMapping(value = "/current")
-    public ResponseEntity<User> getCurrent(Authentication authentication) {
-        return ResponseEntity.ok(
-                ((HashMap<String, User>) authentication.getDetails()).get("account")
-        );
     }
 
 }
